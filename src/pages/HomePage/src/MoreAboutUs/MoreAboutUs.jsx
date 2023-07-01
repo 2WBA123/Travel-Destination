7bf5f8bae120259128e17e55067d8bd114da4697
mohsin-ali-hashmi <mohsinalihashmi.7@gmail.com>
2023-07-02 01:31:32 +0500

    cleaned up the code

--- src/pages/HomePage/src/MoreAboutUs/MoreAboutUs.jsx
+++ src/pages/HomePage/src/MoreAboutUs/MoreAboutUs.jsx
@@ -1,33 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  ABOUT_US_TEXT,
+  BOOK_NOW_TEXT,
+  LIST_ITEMS,
+  TRIP_DESCRIPTION,
+  TRIP_TITLE,
+} from "../../../../constants/AboutUsConstants";
 
 const AboutUsSection = () => {
+
+  const AboutComponent = ({ children, url }) => (
+    <div className="row content">
+      <div className="col-lg-6" data-aos="fade-right" data-aos-delay="100">
+        <img src={url} className="more-about-img" alt="About Us" />
+      </div>
+      {children}
+    </div>
+  );
+
   return (
     <section id="about" className="about">
       <div className="container">
         <div className="section-title text-center" data-aos="fade-left">
-          <h2>More About Us</h2>
+          <h2>{ABOUT_US_TEXT}</h2>
         </div>
 
-        <div className="row content">
-          <div className="col-lg-6" data-aos="fade-right" data-aos-delay="100">
-            <img src="/img/countries/airfares-101_0.webp" className="more-about-img" alt="About Us" />
-          </div>
-          <div className="col-lg-6 pt-4 pt-lg-0" data-aos="fade-left" data-aos-delay="200">
-            <h2>Book your next trip with us!</h2>
-            <h5>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua.
-            </h5>
+        <AboutComponent url={"/img/countries/airfares-101_0.webp"}>
+          <div
+            className="col-lg-6 pt-4 pt-lg-0"
+            data-aos="fade-left"
+            data-aos-delay="200"
+          >
+            <h2>{TRIP_TITLE}</h2>
+            <h5>{TRIP_DESCRIPTION}</h5>
             <ul>
-              <li>✓ Our travel company specializes in creating unforgettable journeys tailored to each individual's unique interests and preferences.</li>
-              <li>✓ With our extensive network of trusted partners and a commitment to exceptional customer service, we strive to provide the ultimate travel experience for every client.</li>
-              <li>✓ Tailored journeys, unforgettable memories – that's our travel company.</li>
-              <li>✓ Exceptional service, curated experiences – your dream vacation starts with us.</li>
+              {LIST_ITEMS.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
             </ul>
-            <Link to="/booking" className="btn-win">BOOK NOW</Link>
+            <Link to="/booking" className="btn-win">
+              {BOOK_NOW_TEXT}
+            </Link>
           </div>
-        </div>
+        </AboutComponent>
       </div>
     </section>
   );
